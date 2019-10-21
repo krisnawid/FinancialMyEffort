b65079d7e33ae205e80bb69bd2a97a773edcfc4c
krisnawid <krisna.widi.kw@gmail.com>
2019-10-21 08:21:10 +0700

    Initial commit

---
+++ app/src/main/java/com/sdkdroid/financialmyeffort/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sdkdroid.financialmyeffort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sdkdroid.financialmyeffort.Activity.BiodataActivity;
+import com.sdkdroid.financialmyeffort.Activity.CetakActivity;
+import com.sdkdroid.financialmyeffort.Activity.LaporanActivity;
+import com.sdkdroid.financialmyeffort.Activity.PemasukanActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handleBiodata(View view) {
+        Intent intent = new Intent(this, BiodataActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlePemasukan(View view) {
+        Intent intent = new Intent(this, PemasukanActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleLaporan(View view) {
+        Intent intent = new Intent(this, LaporanActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleCetak(View view) {
+        Intent intent = new Intent(this, CetakActivity.class);
+        startActivity(intent);
+    }
+}
